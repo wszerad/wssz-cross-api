0214e3dcb26bb8235dcbf70316466ff3fc05cf14
Wszerad <Coralor18$>
2020-03-06 17:10:05 +0100

    direct export of @nestjs decorators

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-export * from './src/decorators';
+export { Controller, ControllerOptions } from '@nestjs/common/decorators/core/controller.decorator';
+export { Body, Param, Query } from '@nestjs/common/decorators/http/route-params.decorator';
+export { Get, Post, All, Head, Options, Patch, Delete, Put, RequestMapping } from '@nestjs/common/decorators/http/request-mapping.decorator';
+export { Return, AttachApi } from './src/decorators';
 export { extractApi } from './src/extract-api';
 export { Endpoint } from './src/Endpoint';
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,7 +1,3 @@
-export { Controller, ControllerOptions } from '@nestjs/common/decorators/core/controller.decorator';
-export { Body, Param, Query } from '@nestjs/common/decorators/http/route-params.decorator';
-export { Get, Post, All, Head, Options, Patch, Delete, Put, RequestMapping } from '@nestjs/common/decorators/http/request-mapping.decorator';
-
 export function AttachApi(apiSchema: any) {
 	return function AttachApiDecorator(target: any) {
 		Object
