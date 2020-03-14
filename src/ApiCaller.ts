271dd20c8f6cc00f7f611022790a350b0873cd0a
Wszerad <Coralor18$>
2020-03-14 20:32:09 +0100

    fix @nestjs import

--- src/ApiCaller.ts
+++ src/ApiCaller.ts
@@ -1,7 +1,7 @@
 import { RETURN_METADATA } from './decorators';
 import { Endpoint } from './Endpoint';
 import { METHOD_METADATA, PATH_METADATA, ROUTE_ARGS_METADATA } from '@nestjs/common/constants';
-import { RequestMethod } from '@nestjs/common';
+import { RequestMethod } from '@nestjs/common/enums/request-method.enum';
 
 export class ApiCaller {
 	constructor(
